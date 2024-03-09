558f620e23220c6fc3cec90b6b9e0e12ba2a23e0
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-09 05:30:18 +0300

    migrations

--- bookoutlet/models.py
+++ bookoutlet/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Books(models.Model):
-    title = 
+    """New Book class"""
+    title = models.CharField(max_length=50)
+    ratings = models.IntegerField()
+    
+    
