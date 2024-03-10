d03fd13dbc1efe5a93b7869e00c32076d9b9a626
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-10 06:14:04 +0300

    update on the code

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,4 @@
-# from django.core import validators
+from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
@@ -6,24 +6,22 @@ from django.utils.text import slugify
 
 # Create your models here.
 
-class Books(models.Model):
-    """New Book class"""
+
+class Book(models.Model):
     title = models.CharField(max_length=50)
-    ratings = models.IntegerField(
+    rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    # Harry Potter 1 => harry-potter-1
     slug = models.SlugField(default="", null=False, db_index=True)
 
     def get_absolute_url(self):
-        return reverse("book_detail", args=[self.slug])
-
+        return reverse("book-detail", args=[self.slug])
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
-
     def __str__(self):
-        return f"{self.title} ({self.ratings})"
-    
+        return f"{self.title} ({self.rating})"
