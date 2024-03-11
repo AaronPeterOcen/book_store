3622cc638cfaefee3dc840100d6fc3e5ab77aaa5
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-11 04:36:32 +0300

    adding one to many relation

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -6,7 +6,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     # pass
-    # readonly_fields = ("slug",)
+    # readonly_fields = ("slug",) 
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating",)
     list_display = ("title", "author",)
